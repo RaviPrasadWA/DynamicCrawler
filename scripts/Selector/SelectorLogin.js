b0a34a17ede0f8d611209b4c9e7f7e1ebb9d708b
Ravi Prasad <ravi.prasad@ness.com>
2017-04-18 23:43:39 +0530

    added the crawler file @TODO:Login incomplete

---
+++ scripts/Selector/SelectorLogin.js
@@ -0,0 +1,15 @@
+var SelectorLogin = {
+
+	canHaveChildSelectors: function () {
+		return true;
+	},
+
+	_getData: function(parentElement) {
+		console.log("SelectorLogin ",parentElement)
+		return true;
+	},
+	
+	getFeatures: function () {
+		return ['usernameSelector','passwordSelector'];
+	}
+}
